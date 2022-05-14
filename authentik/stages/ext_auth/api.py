93ec45336c8cf1be70e89a13ac912e87301aa7e0
Luke Granger-Brown <git@lukegb.com>
2022-05-14 21:24:03 +0000

    Add ext_auth for redirecting users of the web UI.
    This allows me to set a preferred external IdP for users, and have them
    automatically redirected there once they enter their username, without
    exposing the IdP selection on the main identification page.

---
+++ authentik/stages/ext_auth/api.py
@@ -0,0 +1,24 @@
+"""ext_auth Stage API Views"""
+from rest_framework.viewsets import ModelViewSet
+
+from authentik.core.api.used_by import UsedByMixin
+from authentik.flows.api.stages import StageSerializer
+from authentik.stages.ext_auth.models import ExtAuthStage
+
+
+class ExtAuthStageSerializer(StageSerializer):
+    """ExtAuthStage Serializer"""
+
+    class Meta:
+
+        model = ExtAuthStage
+        fields = StageSerializer.Meta.fields
+
+
+class ExtAuthStageViewSet(UsedByMixin, ModelViewSet):
+    """ExtAuthStage Viewset"""
+
+    queryset = ExtAuthStage.objects.all()
+    serializer_class = ExtAuthStageSerializer
+    filterset_fields = "__all__"
+    ordering = ["name"]
